b888f99f3ef206d15f4e0fb70f158eccc769bd44
GauravS43 <gaurav53080@gmail.com>
2022-12-23 22:28:40 -0500

    updated api host

--- src/index.js
+++ src/index.js
@@ -2,38 +2,38 @@ import React from "react"
 import ReactDOM from "react-dom"
 import axios from 'axios'
 import './index.css'
-import {StartScreen} from './startScreen.js'
-import {EndScreen} from './endScreen.js'
-import {GameScreen} from './gameScreen.js'
+import { StartScreen } from './startScreen.js'
+import { EndScreen } from './endScreen.js'
+import { GameScreen } from './gameScreen.js'
 
-function App(){
+function App() {
     const [appState, setAppState] = React.useState(0)
     const [score, setScore] = React.useState(0)
     const [highScore, setHighScore] = React.useState(0)
     const [metricToggle, setMetricToggle] = React.useState(false)
 
-    function startGame(){
+    function startGame() {
         setScore(0)
         setAppState(2)
     }
 
-    function returnToMenu(){
+    function returnToMenu() {
         setAppState(1)
     }
 
     //delay needed for incrementing metric animation to finish
-    function handleLoss(){
+    function handleLoss() {
         setTimeout(() => setAppState(3), 1000)
     }
 
-    function updateScore(){
+    function updateScore() {
         setScore(prevScore => prevScore + 1)
         if (score + 1 > highScore) setHighScore(score + 1)
     }
 
     const [mData, setMData] = React.useState(null)
     React.useEffect(() => {
-        axios.get("https://mangahlapi.herokuapp.com/")
+        axios.get("http://gauravs43.pythonanywhere.com/")
             .then(res => {
                 setMData(res.data)
                 setAppState(1)
@@ -41,11 +41,11 @@ function App(){
     }, [])
 
     return (
-        <div className={appState !== 3 ? "wrapper" : "" }>
+        <div className={appState !== 3 ? "wrapper" : ""}>
             {appState === 0 && <div className="loading"></div>}
             {appState === 1 && <StartScreen mData={mData} metricToggle={metricToggle} setMetricToggle={setMetricToggle} handleClick={startGame} />}
-            {appState === 2 && <GameScreen mData={mData} metricToggle={metricToggle} score={score} highScore={highScore} handleLoss={handleLoss} handleScore={updateScore}/>}
-            {appState === 3 && <EndScreen mData={mData} score={score} highScore={highScore} startGame={startGame} returnToMenu={returnToMenu}/>}
+            {appState === 2 && <GameScreen mData={mData} metricToggle={metricToggle} score={score} highScore={highScore} handleLoss={handleLoss} handleScore={updateScore} />}
+            {appState === 3 && <EndScreen mData={mData} score={score} highScore={highScore} startGame={startGame} returnToMenu={returnToMenu} />}
         </div>
     )
 }
